ed745a9ab640a84c56df8948b90737e9f2e3e6af
practice <aditi.pathak_cs22@gla.ac.in>
2026-02-23 22:39:05 +0530

    UC3: Refactor to generic QuantityLength class with enum

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
+        QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCH);
+
+        bool result = q1.Equals(q2);
+
+        Console.WriteLine("Are Equal? " + result);
+    }
+}
