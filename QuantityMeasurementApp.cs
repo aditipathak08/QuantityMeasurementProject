36eebea4d2e3083ae5ea3b396743a32f43514c67
practice <aditi.pathak_cs22@gla.ac.in>
2026-02-23 21:42:34 +0530

    UC2: Add Inches equality and static check methods

--- QuantityMeasurementApp.cs
+++ QuantityMeasurementApp.cs
@@ -1,8 +1,7 @@
 using System;
-
 class QuantityMeasurementApp
 {
-    public class Feet
+    public class Feet//5 ft table1 equals 5 ft table2
     {
         private double value;
 
@@ -11,24 +10,62 @@ class QuantityMeasurementApp
             this.value = value;
         }
 
-        public override bool Equals(object obj)//== compares reference and not values
+        public override bool Equals(object obj)
         {
             if (obj == null)
                 return false;
+
             if (!(obj is Feet))
-                return false;//means object==feet which is true
-            Feet other = (Feet)obj;//converting that object into feet
-            return this.value == other.value;//val1==val2
-     }
+                return false;
+
+            Feet other = (Feet)obj;
+
+            return this.value == other.value;
+        }
+    }
+    public class Inches//5 inches table1 equals 5 inches table2
+    {
+        private double value;
+
+        public Inches(double value)
+        {
+            this.value = value;
+        }
+
+        public override bool Equals(object obj)//we override because == compares references and not values
+        {
+            if (obj == null)
+                return false;
+
+            if (!(obj is Inches))
+                return false;
+
+            Inches other = (Inches)obj;
+
+            return this.value == other.value;
+        }
+    }
+    public static bool CheckFeet(double v1, double v2)
+    {
+        Feet f1 = new Feet(v1);
+        Feet f2 = new Feet(v2);
+
+        return f1.Equals(f2);
+    }
+    public static bool CheckInches(double v1, double v2)
+    {
+        Inches i1 = new Inches(v1);
+        Inches i2 = new Inches(v2);
+
+        return i1.Equals(i2);
     }
 
     static void Main()
     {
-        Feet f1 = new Feet(1.0);
-        Feet f2 = new Feet(1.0);
+        bool feetResult = CheckFeet(1.0, 1.0);
+        Console.WriteLine("Feet Equal? " + feetResult);
 
-        bool result = f1.Equals(f2);
-
-        Console.WriteLine(result);
+        bool inchesResult = CheckInches(1.0, 1.0);
+        Console.WriteLine("Inches Equal? " + inchesResult);
     }
 }
